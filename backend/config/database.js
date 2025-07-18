f64063032f9200b590f3f076b1623425a6cd1433
Thiago <thi4god777@gmail.com>
2025-07-18 11:10:58 -0300

    att

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('meuprojeto', 'root', '123456', {
   host: 'localhost',
   dialect: 'mysql',
-  logging: false, // opcional, para não mostrar logs SQL no console
+  logging: false,
   define: {
     timestamps: false // para não criar colunas createdAt e updatedAt automaticamente
   }
