dacedf434ace27963815e209aeaab9d81531f9a7
Rafael Berg Medeiros Miranda <rafaelberg32@gmail.com>
2025-07-28 20:58:26 -0300

    add TypeScript; create libs/axios for base URL and services for requests

---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css'
+
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('Elemento root não encontrado no DOM');
+}
+
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
