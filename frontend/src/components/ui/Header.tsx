06fcee3d945402502c723b1f2ae3d0e3c7b0cabd
Rafael Berg Medeiros Miranda <rafaelberg32@gmail.com>
2025-07-30 11:19:18 -0300

    feat: create landing page

---
+++ frontend/src/components/ui/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="sticky top-0 z-50 flex bg-[#f7fafc] gap-4 self-start w-full border-b-[1px] border-[#e8eaed] py-2 items-center">
+            {/* trocar */}
+            <img src="icon.jpg" className="w-4" />
+            <h3>BookHaven</h3>
+        </header>
+    )
+}
