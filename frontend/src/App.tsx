7ec82b494534fdbaeee54948c0d4c974752c3ba8
Rafael Berg Medeiros Miranda <rafaelberg32@gmail.com>
2025-07-31 11:35:08 -0300

    feat: add book management page, sidebar UI and implement middleware for admin page protection.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from './view/LandingPage';
-import LivroApp from './view/LivroApp';
 import NotFoundPage from './view/NotFoundPage';
 import AuthPage from './view/AuthPage';
+import AuthMiddleware from './middleware/AuthMiddleware';
+import DashBoardPage from './view/admin/DashBoardPage';
+import BookManagementPage from './view/admin/BookManagementPage';
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/dashboard" element={<LivroApp />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="*" element={<NotFoundPage />} />
+      <Route element={<AuthMiddleware />}>
+        <Route path="/dashboard" element={<DashBoardPage />} />
+        <Route path="/book-management" element={<BookManagementPage />} />
+      </Route>
     </Routes>
   )
 }
---
+++ frontend/src/view/admin/DashBoardPage.tsx
@@ -0,0 +1,6 @@
+export default function DashBoardPage() {
+    return(
+        <p>START</p>
+    )
+}
+    
