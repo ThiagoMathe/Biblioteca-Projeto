7eba7bdc28653552cd37c97eba7cbca9e312ceee
Thiago <thi4god777@gmail.com>
2025-08-01 16:50:40 -0300

    ajustando backend

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || 'chave_secreta_padrao_para_dev',
+  JWT_SECRET: process.env.JWT_SECRET || 'chave_secreta',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1h'
 };
